ecfb0fb80025cabd092905963bd7207f83c029fd
Leonid <jiehuh87@yandex.ru>
2020-12-21 22:46:52 +0300

    Les2 Ex1-3

--- Les1/Les2Ex2/Program.cs
+++ Les1/Les2Ex2/Program.cs
@@ -15,13 +15,16 @@ namespace Les2Ex2
         //c. *Рекурсивно, используя свойство чётности степени.
         static void Main(string[] args)
         {
+            int count_a = 0;
+            int count_b = 0;
+            int count_c = 0;
             Console.Write("Введите число: ");
             int a = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите степнь: ");
             int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"{a}^{n} = {Pow_a(a,n)}");
-            Console.WriteLine($"{a}^{n} = {Pow_b(a,n)}");
-            Console.WriteLine($"{a}^{n} = {Pow_c(a,n)}");
+            Console.WriteLine($"{a}^{n} = {Pow_a(a,n,ref count_a)}, кол-во операций: {count_a}");
+            Console.WriteLine($"{a}^{n} = {Pow_b(a,n, ref count_b)}, кол-во операций: {count_b}");
+            Console.WriteLine($"{a}^{n} = {Pow_c(a,n, ref count_c)}, кол-во операций: {count_c}");
             Console.ReadKey();
         }
         /// <summary>
@@ -30,13 +33,14 @@ namespace Les2Ex2
         /// <param name="a">число</param>
         /// <param name="n">степень</param>
         /// <returns></returns>
-        static double Pow_a(int a, int n)
+        static double Pow_a(int a, int n, ref int count)
         {
             double num_n = 1;
             if (n >= 0)
             {
                 for (int i = 0; i < n; i++)
                 {
+                    count++;
                     num_n *= a;
                 }
                 return num_n; 
@@ -45,6 +49,7 @@ namespace Les2Ex2
             {
                 for (int i = 0; i < -n; i++)
                 {
+                    count++;
                     num_n *= a;
                 }
                 return 1/num_n;
@@ -57,11 +62,12 @@ namespace Les2Ex2
         /// <param name="a">число</param>
         /// <param name="n">степень</param>
         /// <returns></returns>
-        static double Pow_b(int a, int n)
+        static double Pow_b(int a, int n, ref int count)
         {
-            if (n > 0) return a * Pow_b(a, n - 1);
+            count++;
+            if (n > 0) return a * Pow_b(a, n - 1, ref count);
             else if (n == 0) return 1;
-            else return 1 / (a * Pow_b(a, -n - 1));
+            else return 1 / (a * Pow_b(a, -n - 1, ref count));
         }
         /// <summary>
         /// возведение в степень с рекурсией, используя свойство чётности степени.
@@ -69,26 +75,27 @@ namespace Les2Ex2
         /// <param name="a">число</param>
         /// <param name="n">степень</param>
         /// <returns></returns>
-        static double Pow_c(int a, int n)
+        static double Pow_c(int a, int n, ref int count)
         {
+            count++;
             if (n > 0)
             { 
                 if (n % 2 == 0)
                 {
-                    double p = Pow_c(a, n / 2);
+                    double p = Pow_c(a, n / 2, ref count);
                     return p * p;
                 }
-                else return a * Pow_c(a, n - 1);
+                else return a * Pow_c(a, n - 1, ref count);
             }
             else if (n == 0) return 1;
             else 
             {
                 if (n % 2 == 0)
                 {
-                    double p = Pow_c(a, -n / 2);
+                    double p = Pow_c(a, -n / 2, ref count);
                     return 1/(p * p);
                 }
-                else return 1/(a * Pow_c(a, -n - 1));
+                else return 1/(a * Pow_c(a, -n - 1, ref count));
             }
         }
     }
---
+++ Les1/Les2Ex3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les2Ex3
+{
+    class Program
+    {
+        //Ронжин Л.
+        //3. **Исполнитель «Калькулятор» преобразует целое число, записанное на экране. У
+        //исполнителя две команды, каждой присвоен номер:
+        //1. Прибавь 1.
+        //2. Умножь на 2.
+        //Первая команда увеличивает число на экране на 1, вторая увеличивает его в 2 раза.Сколько
+        //существует программ, которые число 3 преобразуют в число 20:
+        //а. С использованием массива.
+        //b. * С использованием рекурсии.
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число которое необходимо получить: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            Covert(a, b, a.ToString(), ref count);
+            Console.WriteLine($"Кол-во вариантов: {count}");
+            Console.ReadKey();
+        }
+
+        private static void Covert(int a, int b, string str, ref int count)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                string s = str;
+                int temp = a;
+                if (i == 0)
+                {
+                    temp++;
+                    s += "+1";
+                }
+                else
+                {
+                    temp *= 2;
+                    s += "*2";
+                }
+                if (temp < b) Covert(temp, b, s, ref count);
+                else if (temp == b) Console.WriteLine($"{++count}) {s} + = + {b}");
+            }
+        }
+    }
+}
