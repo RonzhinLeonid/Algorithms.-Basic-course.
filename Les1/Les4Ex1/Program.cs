c9cdf315638030b48d33120d865450ba73958d63
Leonid <jiehuh87@yandex.ru>
2021-01-10 19:43:43 +0300

    Les4 Ex1,2

---
+++ Les1/Les4Ex1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les4Ex1
+{
+    class Program
+    {
+        //Ронжин Л.
+        //Количество маршрутов с препятствиями. Реализовать чтение массива с препятствием и
+        //нахождение количество маршрутов
+        //Например, карта:
+        //3 3
+        //1 1 1
+        //0 1 0
+        //0 1 0
+
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int m = 3;
+            int[,] map = {  {1,1,1},
+                            {0,1,0},
+                            {0,1,0} };
+            
+            int[,] arr = new int[n,m];
+            for (int i = 0; i < m; i++)
+            {
+                arr[0, i] = 1;
+            }
+            for (int i = 1; i < n; i++)
+            {
+                if (map[i, 0] == 1) arr[i, 0] = 1;
+                else arr[i, 0] = 0;
+
+                for (int j = 1; j < m; j++)
+                {
+                    if (map[i, j] == 1) arr[i, j] = arr[i, j - 1] + arr[i - 1, j];
+                    else arr[i, j] = 0;
+                }
+            }
+            Print(arr);
+            Console.ReadLine();
+        }
+        public static void Print(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
---
+++ Les1/Les4Ex3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les4Ex3
+{
+
+    //Ронжин Л.
+    //3. ***Требуется обойти конём шахматную доску размером NxM, пройдя через все поля доски по
+    //одному разу.Здесь алгоритм решения такой же как и в задаче о 8 ферзях.Разница только в проверке
+    //положения коня.
+
+    class Program
+    {
+            //static int[,] Ar = new int[6, 6];
+            //static int n;
+            static void Main(string[] args)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        int[,] Ar = new int[5, 5];
+                        int n = 0;
+                        Move(i, j, Ar, n);
+                        Console.WriteLine("-------------------------");
+                    }
+                
+                }
+
+                Console.ReadLine();
+            }
+            static void Move(int X, int Y, int[,] Ar, int n)
+            {
+                int iX = 0, iY = 0, nn = ++n;
+                bool WasStep = false;
+                Ar[X, Y] = 1;
+                for (int i = 0; i < 8; i++)
+                {
+                    switch (i)
+                    {
+                        case 0: iX = X + 1; iY = Y - 2; break;
+                        case 1: iX = X + 2; iY = Y + 1; break;
+                        case 2: iX = X - 1; iY = Y + 2; break;
+                        case 3: iX = X - 2; iY = Y - 1; break;
+                        case 4: iX = X - 1; iY = Y - 2; break;
+                        case 5: iX = X + 2; iY = Y - 1; break;
+                        case 6: iX = X + 1; iY = Y + 2; break;
+                        case 7: iX = X - 2; iY = Y + 1; break;
+                    }
+                    if (iX > -1 && iX < 5 && iY > -1 && iY < 5 && Ar[iX, iY] == 0)
+                    {
+                        WasStep = true;
+                        Console.WriteLine("{0,4}:  {1}-{2}  ->   {3}-{4}", "(" + nn + ")", X + 1, Y + 1, iX + 1, iY + 1);
+                        Move(iX, iY, Ar, n);
+                    }
+                }
+                if (!WasStep) n--;
+            }
+        
+    }
+}
