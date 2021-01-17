75846ed6dcdfd60de841c6c6e0079092cbd3637e
Leonid <jiehuh87@yandex.ru>
2021-01-17 18:24:09 +0300

    Les6Ex1

---
+++ Les1/Les6Ex1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les6Ex1
+{
+    class Program
+    {
+        //Ронжин Л.
+        //1. Реализовать простейшую хэш-функцию. На вход функции подается строка, на выходе сумма кодов символов
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+
+            Console.Write(Hash(str));
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Хэш-функция
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static int Hash(string x)
+        {
+            int rez = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                rez += (int)Convert.ToChar(x[i]);
+            }
+            return rez;
+        }
+    }
+}
